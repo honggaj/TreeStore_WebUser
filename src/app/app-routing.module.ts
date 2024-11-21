f3d9ce59353c8352489b7c4e0cfee4b4e34813e7
Ngô Gia Bảo <ngobao0419@gmail.com>
2024-11-21 20:16:49 +0700

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,51 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { IntroduceComponent } from './components/introduce/introduce.component';
-import { ProductComponent } from './components/product/product.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { CartComponent } from './components/cart/cart.component';
-import { OrderComponent } from './components/order/order.component';
-import { LoginUserComponent } from './components/login-user/login-user.component';
-import { RegisterUserComponent } from './components/register-user/register-user.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { OrderHistoryComponent } from './components/order-history/order-history.component';
-import { OtpVerificationComponent } from './components/checkEmail/checkEmail.components';
-import { AccountComponent } from './components/account-user/account-user.component';
-import { OrderDetailComponent } from './components/order-detail/order-detail.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export const routes: Routes = [
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
   // Đường dẫn cho trang đăng nhập và đăng ký
-  { path: 'dangnhap', component: LoginUserComponent },
-  { path: 'dangky', component: RegisterUserComponent },
+  { path: 'dangnhap', loadComponent: () => import('./components/login-user/login-user.component').then(m => m.LoginUserComponent) },
+  { path: 'dangky', loadComponent: () => import('./components/register-user/register-user.component').then(m => m.RegisterUserComponent) },
 
   // Các trang chính
-  { path: 'home', component: HomeComponent },
-  { path: 'gioithieu', component: IntroduceComponent },
-  { path: 'sanpham', component: ProductComponent },
-  { path: 'lienhe', component: ContactComponent },
-  { path: 'giohang', component: CartComponent },
-  { path: 'dathang', component: OrderComponent },
+  { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'gioithieu', loadComponent: () => import('./components/introduce/introduce.component').then(m => m.IntroduceComponent) },
+  { path: 'sanpham', loadComponent: () => import('./components/product/product.component').then(m => m.ProductComponent) },
+  { path: 'lienhe', loadComponent: () => import('./components/contact/contact.component').then(m => m.ContactComponent) },
+  { path: 'giohang', loadComponent: () => import('./components/cart/cart.component').then(m => m.CartComponent) },
+  { path: 'dathang', loadComponent: () => import('./components/order/order.component').then(m => m.OrderComponent) },
 
   // Lịch sử đơn hàng và chi tiết đơn hàng
-  { path: 'lichsudonhang', component: OrderHistoryComponent },
-  { path: 'order-detail/:orderId', component: OrderDetailComponent }, // Trang chi tiết đơn hàng
+  { path: 'lichsudonhang', loadComponent: () => import('./components/order-history/order-history.component').then(m => m.OrderHistoryComponent) },
+  { path: 'order-detail/:orderId', loadComponent: () => import('./components/order-detail/order-detail.component').then(m => m.OrderDetailComponent) }, // Trang chi tiết đơn hàng
 
   // Quên mật khẩu và xác minh OTP
-  { path: 'quenmatkhau', component: ForgotPasswordComponent },
-  { path: 'checkEmail', component: OtpVerificationComponent },
+  { path: 'quenmatkhau', loadComponent: () => import('./components/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent) },
+  { path: 'checkEmail', loadComponent: () => import('./components/checkEmail/checkEmail.components').then(m => m.OtpVerificationComponent) },
 
   // Tài khoản người dùng
-  { path: 'taikhoan', component: AccountComponent },
+  { path: 'taikhoan', loadComponent: () => import('./components/account-user/account-user.component').then(m => m.AccountComponent) },
 
   // Trang đánh giá sản phẩm
-  { path: 'danhgia/:id', loadComponent: () => import('./components/review/review-user.component').then(c => c.ReviewComponent) },
+  { path: 'danhgia/:id', loadComponent: () => import('./components/review/review-user.component').then(m => m.ReviewComponent) },
 
   // Chi tiết sản phẩm sử dụng lazy loading
   { 
     path: 'xemchitiet/:id',
-    loadComponent: () => import('./components/product-detail/product-detail.component').then(c => c.ProductDetailComponent)
+    loadComponent: () => import('./components/product-detail/product-detail.component').then(m => m.ProductDetailComponent)
   },
 ];
 
